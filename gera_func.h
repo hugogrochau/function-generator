53b147dca5ea9c09fd07474a94cb1b5f8fc94522
Hugo <hugo@grochau.com>
2014-06-06 20:32:57 -0300

    Fix gera_func.h indentation

--- gera_func.h
+++ gera_func.h
@@ -4,19 +4,18 @@
 typedef enum {INT_PAR, CHAR_PAR, DOUBLE_PAR, PTR_PAR} TipoParam;
 
 typedef struct {
-   TipoParam tipo;  /* indica o tipo do parametro */
-   int amarrado; /* indica se o parametro deve ter um valor constante */
-   int posicao; /* indica um número entre <code>1</code>  e <code>n</cod> */
-   union {
-      int v_int;
-      char v_char;
-      double v_double;
-      void* v_ptr;
-   } valor;         /* define o valor do parametro se este for constante */
+    TipoParam tipo;  /* indica o tipo do parametro */
+    int amarrado; /* indica se o parametro deve ter um valor constante */
+    int posicao; /* indica um número entre <code>1</code>  e <code>n</cod> */
+    union {
+        int v_int;
+        char v_char;
+        double v_double;
+        void *v_ptr;
+    } valor;         /* define o valor do parametro se este for constante */
 } Parametro;
 
-void* gera_func (void* f, int n, Parametro params[]);
-void libera_func (void* func);
+void *gera_func(void *f, int n, Parametro params[]);
+void libera_func(void *func);
 
 #endif
-
